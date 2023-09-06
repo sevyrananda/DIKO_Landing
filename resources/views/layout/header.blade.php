a50af6368eae4f6acc8c962a95fb725a124b1c92
derisr <rizkiderisarasvindi@student.ac.id>
2023-09-06 16:30:27 +0700

    update

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,5 +1,5 @@
  <!-- ======= Header ======= -->
- <header id="header" class="header fixed-top" data-scrollto-offset="0">
+ <header id="header" class="header fixed-top"  style="background-color:#C3E2FF;" data-scrollto-offset="0">
     <div class="container-fluid d-flex align-items-center justify-content-between">
 
       <a href="" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
@@ -17,10 +17,10 @@
         <ul>
           <li><a class="nav-link scrollto" href="index.html#about" >Beranda</a></li>
           <li><a class="nav-link scrollto" href="index.html#features">Fitur</a></li>
-          <li><a class="nav-link scrollto" href="index.html#pricing">Harga</a></li>
           <!-- <li><a class="nav-link scrollto" href="index.html#team">Blog</a></li> -->
           <!-- <li><a href="blog.html">Blog</a></li> -->
           <li><a class="nav-link scrollto" href="index.html#testimonials">Testimonial</a></li>
+          <li><a class="nav-link scrollto" href="index.html#pricing">Harga</a></li>
           <li><a class="nav-link scrollto" href="index.html#contact">Contact</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle d-none"></i>
