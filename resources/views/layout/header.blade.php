8303baa724f36637e387f0319146e6504a1580e0
Najwa Ainaya <112097306+najwaainaya@users.noreply.github.com>
2023-10-06 11:37:17 +0700

    update

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -2,7 +2,7 @@
  <header id="header" class="header fixed-top"  style="background-color:#C3E2FF;" data-scrollto-offset="0">
     <div class="container-fluid d-flex align-items-center justify-content-between">
 
-      <a href="" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
+      <a href="{{ route('landing') }}" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
         <div style="display: flex; margin-left: 50px;">
           <img src="{{url('HeroBiz')}}/assets/img/DIKO TRANSPARANT.png" alt="" width="40" height="70">
           <h3 style="margin-left: 5px;">
@@ -21,6 +21,6 @@
         </ul>
         <i class="bi bi-list mobile-nav-toggle d-none"></i>
       </nav><!-- .navbar -->
-      <a class="btn-getstarted scrollto" style="margin-right: 50px; background-color:#144B9A;" href="{{ route('landing') }}">DOWNLOAD</a>
+      <a class="btn-getstarted scrollto" style="margin-right: 50px; background-color:#144B9A;" href="">DOWNLOAD</a>
     </div>
   </header><!-- End Header -->
--- resources/views/layout2/footer.blade.php
+++ resources/views/layout2/footer.blade.php
@@ -1,80 +1,72 @@
 <!-- ======= Footer ======= -->
 <footer id="footer">
 
-<div class="footer-top">
-  <div class="container">
-    <div class="row">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
 
-      <div class="col-lg-3 col-md-6 footer-contact">
-        <h3>DIKO</h3>
-        <p>
-          Office: JL. Margatama Asri IV No. 3<br>
-          Kota Madiun<br>
-          Jawa Timur<br><br>
-          <strong>Phone:</strong> 0811-3636-09 <br>
-          <strong>Email:</strong> cs@diko.co.id<br>
-        </p>
-      </div>
+                <div class="col-lg-3 col-md-6 footer-contact">
+                    <h3>DIKO</h3>
+                    <p>
+                        Office: JL. Margatama Asri IV No. 3<br>
+                        Kota Madiun<br>
+                        Jawa Timur<br><br>
+                        <strong>Phone:</strong> 0811-3636-09 <br>
+                        <strong>Email:</strong> cs@diko.co.id<br>
+                    </p>
+                </div>
 
-      <div class="col-lg-3 col-md-6 footer-links">
-        <h4>Tentang DIKO</h4>
-        <ul>
-          <li><i class="bx bx-chevron-right"></i><a href="#">Perusahaan</a></li>
-          <li><i class="bx bx-chevron-right"></i><a href="#">Partner Resmi</a></li>
-        </ul>
-        <ul>
-          <li>
-            <img 
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Logo_of_Ministry_of_Communication_and_Information_Technology_of_the_Republic_of_Indonesia.svg/2060px-Logo_of_Ministry_of_Communication_and_Information_Technology_of_the_Republic_of_Indonesia.svg.png" 
-              class="img-fluid animated" 
-              alt=""  
-              style="height:55px; padding:10px; margin-top:50px;"
-              >
-            <img src="https://idxcoop.kemenkopukm.go.id/images/logo-idxcoop.png" 
-              class="img-fluid animated" 
-              alt="" 
-              style="height:55px; padding:10px; margin-top:50px"
-            >
-            <img src="https://qms-consulting.id/wp-content/uploads/2020/12/1_G1Yc_22hHBfgGlNdRJpBDw.jpeg" 
-              class="img-fluid animated" 
-              alt="" 
-              style="height:55px; padding:10px; margin-top:50px"
-            >
-            <img src="https://1.bp.blogspot.com/-uprDrIHInls/X9N5GuSqiFI/AAAAAAAAHkM/8CMLFZSv-IIbwo4-vbGeo5xubP8riGhigCLcBGAsYHQ/s1000/bangga%2Bbuatan%2Bindonesia-01.png" 
-              class="img-fluid animated" 
-              alt="" 
-              style="height:55px; padding:10px; margin-top:50px"
-            >
-          </li>
-        </ul>
-      </div>
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Tentang DIKO</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i><a href="#">Perusahaan</a></li>
+                        <li><i class="bx bx-chevron-right"></i><a href="#">Partner Resmi</a></li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Logo_of_Ministry_of_Communication_and_Information_Technology_of_the_Republic_of_Indonesia.svg/2060px-Logo_of_Ministry_of_Communication_and_Information_Technology_of_the_Republic_of_Indonesia.svg.png"
+                                class="img-fluid animated" alt=""
+                                style="height:55px; padding:10px; margin-top:50px;">
+                            <img src="https://idxcoop.kemenkopukm.go.id/images/logo-idxcoop.png"
+                                class="img-fluid animated" alt=""
+                                style="height:55px; padding:10px; margin-top:50px">
+                            <img src="https://qms-consulting.id/wp-content/uploads/2020/12/1_G1Yc_22hHBfgGlNdRJpBDw.jpeg"
+                                class="img-fluid animated" alt=""
+                                style="height:55px; padding:10px; margin-top:50px">
+                            <img src="https://1.bp.blogspot.com/-uprDrIHInls/X9N5GuSqiFI/AAAAAAAAHkM/8CMLFZSv-IIbwo4-vbGeo5xubP8riGhigCLcBGAsYHQ/s1000/bangga%2Bbuatan%2Bindonesia-01.png"
+                                class="img-fluid animated" alt=""
+                                style="height:55px; padding:10px; margin-top:50px">
+                        </li>
+                    </ul>
+                </div>
 
-      <div class="col-lg-3 col-md-6 footer-links">
-        <h4>Produk</h4>
-        <ul>
-          <li><i class="bx bx-chevron-right"></i> <a href="{{ route('sp') }}">DIKO SP</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="{{ route('pos') }}">DIKO POS</a></li>
-        </ul>
-      </div>
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Produk</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="{{ route('sp') }}">DIKO SP</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="{{ route('pos') }}">DIKO POS</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
 
-<div class="container">
 
-  <div class="copyright-wrap d-md-flex py-4">
-    <div class="me-md-auto text-center text-md-start">
-      <div class="copyright">
-        &copy; Copyright <strong><span>DIKO</span></strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-      </div>
+    <div class="container">
+
+        <div class="copyright-wrap d-md-flex py-4">
+            <div class="me-md-auto text-center text-md-start">
+                <div class="copyright">
+                    &copy; Copyright <strong><span>DIKO</span></strong>. All Rights Reserved
+                </div>
+                <div class="credits">
+                </div>
+            </div>
+            <div class="social-links text-center text-md-right pt-3 pt-md-0">
+                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+            </div>
+        </div>
     </div>
-    <div class="social-links text-center text-md-right pt-3 pt-md-0">
-      <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-      <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-      <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-    </div>
-  </div>
-</div>
 </footer><!-- End Footer -->
